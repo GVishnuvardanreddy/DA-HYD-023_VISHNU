bc2a8b419f95c40d63693e4fcabb3c28e876101c
GVishnuvardanreddy <vishnuvardanreddy4146@gmail.com>
2026-08-18 15:55:18 +0530

    task-lists,tuples,sets

--- 16aug-task.py
+++ 16aug-task.py
@@ -1,148 +1,137 @@
-#Question 1: Student Marks Manager
+
+#task------>1
 '''
 marks=[]
 for i in range(3):
-    mark=int(input('enter the marks:'))
-    marks.append(mark)
-
-marks.insert(0,90)
-marks.extend([75,85])
-a=(75 in marks)
-#print(a)
-if a == True:
-    marks.remove(75)
-print("removed value is:",marks.pop())
-
+    x=int(input("Enter the marks: "))
+    marks.append(x)
 print(marks)
-print(len(marks))
+marks.insert(0,90)
+marks.extend((75,85))
+print(marks)
+if 75 in marks:
+    marks.remove(75)
+print("Poped value: ",marks.pop())
+print("Final list:",marks)
+print("Length of the list:",len(marks))
 '''
 
-#Question 2: Number List Analyser
 
+#task--------->2
 '''
-numbers=[20, 10, 30, 20, 40, 20]
+numbers=[20,10,30,40,20]
+print(numbers)
 numbers.sort()
-print(numbers)
-numbers.sort(reverse=True)
-print(numbers)
-search=int(input('enter the number:'))
-if search in numbers:
-           print('Number found')
-           print('count of the search number:',numbers.count(search))
-           print('index:',numbers.index(search))
-else:
-    print('not found')
-print('minimum number is:',min(numbers))
-print('maximum number is:',max(numbers))   
-print('sum of all numbers is:',sum(numbers))        
+print("ascending order list:")
+for i in numbers:
+    print(i)
+numbers.reverse()
+print("descending order list:")
+for i in numbers:
+    print(i)
+x=int(input("Enter the number: "))
+if x in numbers:
+    print("count and index: ",numbers.count(x),numbers.index(x))
+print("minimum number in the list:",min(numbers))
+print("maximun number in the list:",max(numbers))
+print("sum of the list:",sum(numbers))
 '''
 
-#Question 3: Even and Odd Number Separator 
-
+#task---->3
 '''
 numbers=[10,15,20,25,30,35]
 even=[]
 odd=[]
-for number in numbers:
-    if number % 2 == 0:
-        even.append(number)
+for i in numbers:
+    if i%2==0:
+        even.append(i)
     else:
-        odd.append(number)
-
-print('Even list:',even)
-print('Odd list:',odd)
-#slicing
-print('first three:',numbers[:3])
-print('last three:',numbers[-3:])
-
-backup_list=numbers.copy()
-print('backup_list:',backup_list)
+        odd.append(i)
+print("even list:",even)
+print("odd list:",odd)
+print("first 3 numbers:",numbers[:3])
+print("first 3 numbers:",numbers[3:])
+copy_numbers=numbers.copy()
 numbers.clear()
-print('original list:',numbers)
+print("original numbers:",numbers)
+print("copy numbers:",copy_numbers)
 '''
 
-
-#Question 4: Unique Name Manager
-
-
+#task---->4
 '''
-names = ["Asha", "Rahul", "Asha", "John", "Rahul"]
-set_names=set(names) #Asha, Rahul, John
-print(set_names)
-
-set_names.add("Meera")
-print(set_names)
-set_names.update(("Arun","priya"))
-print(set_names)
-if "John" in set_names:
-    set_names.remove("John")
-    #print(set_names)
-
-    set_names.discard("David")
-    print(set_names)
-for i in set_names:
+names=['Asha','Rahul','Asha','John','Rahul']
+x=set(names)
+print(x)
+x.add('Meera')
+print(x)
+x.update(("Arun","Priya"))
+print(x)
+if 'John' in x:
+    x.remove("John")
+print(x)
+x.discard('John')
+for i in x:
     print(i)
 '''
 
-#Question 5: Course Student Comparison
-
-python_students = {"Asha", "Rahul", "John", "Meera"} 
-da_students = {"Rahul", "Meera", "Arun"}
-
+#task------>5
 '''
-students=python_students.union(da_students)
-common=python_students.intersection(da_students)
-difference=python_students.difference(da_students)
-#difference=da_students.difference(python_students)
-sym=python_students.symmetric_difference(da_students)
-print(da_students.issubset(python_students))
-print(python_students.issuperset(da_students))
-print(python_students.isdisjoint(da_students))
-print(students)
-print(common)
-print(difference)
-print(sym)
+python_students={'Asha','Rahul','Meera','John'}
+da_students={'Asha','Meera','Arun'}
+x=python_students.union(da_students)
+print('total students:')
+for i in x:
+    print(i)
+y=python_students.intersection(da_students)
+print('students who are taking both course:')
+for i in y:
+    print(i)
+z=python_students.difference(da_students)
+print("students who taking only python course:")
+for i in z:
+    print(i)
+a=python_students ^ (da_students)
+print("students who taking only one course:")
+for i in a:
+    print(i)
+
+print('is the da set is a subset of python set???:',da_students.issubset(python_students))
+print('is the da set is a superset of python set???:',da_students.issuperset(python_students))
+print('is the da set is a subset of python set???:',da_students.isdisjoint(python_students))
 '''
-
-all_students = python_students.union(da_students)
-print("Students from both courses:")
-for student in all_students:
-    print(student)
-
-
-both = python_students.intersection(da_students)
-print("\nStudents learning both courses:")
-for student in both:
-    print(student)
-
-
-only_python=python_students.difference(da_students)
-print("\nStudents learning only python courses:")
-for students in only_python:
-    print(students)
-
-
-only_DataAnalysis=da_students.difference(python_students)
-print("\nStudents learning only Data Analysis courses:")
-for students in only_DataAnalysis:
-    print(students)
-    
-one_course=python_students.symmetric_difference(da_students)
-print("\nStudents learning only one courses:")
-for student in one_course:
-    print(student)
-
+                      #or#
+'''
 if da_students.issubset(python_students):
-    print("\nDA is a subset of Python: True")
+    print('da set is the subset of python set')
 else:
-    print("\nDA is a subset of Python: False")
+    print('da set is not subset of python set')
 
-if python_students.issuperset(da_students):
-    print("Python is a superset of DA: True")
+if da_students.issuperset(python_students):
+    print('da set is the superset of python set')
 else:
-    print("Python is a superset of DA: False")
+    print('da set is not superset of python set')
 
-if python_students.isdisjoint(da_students):
-    print("The sets are disjoint: True")
+if da_students.isdisjoint(python_students):
+    print('da set is the disjoint of python set')
 else:
-    print("The sets are disjoint: False")
+    print('da set is not disjoint of python set')
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
